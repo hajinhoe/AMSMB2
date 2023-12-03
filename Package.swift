515badf50f80e974ba3e0ea644002e7e9edd9ab3
Jinho <hajinho@hanyang.ac.kr>
2023-12-03 17:15:28 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     products: [
         .library(
             name: "AMSMB2",
-            type: .dynamic,
+            //type: .dynamic,
             targets: ["AMSMB2"]
         ),
     ],
